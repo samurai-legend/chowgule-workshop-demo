849bfcf9dc2ad4c63cb9023113f726423248799f
Suyog Mishal <mishal.s.suyog@gmail.com>
2023-09-30 10:02:39 +0530

    updated env example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import CreatePostSection from "@app/components/CreatePostSection";
 import PostCard from "@app/components/PostCard";
+import { authOptions } from "@app/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function RootRoute() {
+export default async function RootRoute() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
       <div className="container">
         <CreatePostSection />
-        <div className="grid grid-cols-2 gap-10">
+        <h2>Server Session</h2>
+        <pre>{JSON.stringify(session)}</pre>
+        {/* <div className="grid grid-cols-2 gap-10">
           <PostCard
             title="Test Post"
             description="Test Desc."
@@ -21,7 +27,7 @@ export default function RootRoute() {
             imageUrl="/images/profile.jpg"
             author={{ name: "Mahesh Sir" }}
           />
-        </div>
+        </div> */}
       </div>
     </main>
   );
--- src/components/CreatePostSection.tsx
+++ src/components/CreatePostSection.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { signIn, signOut } from "next-auth/react";
 import Button from "./Button";
 
 const CreatePostSection: React.FC = () => {
@@ -7,13 +8,7 @@ const CreatePostSection: React.FC = () => {
       className="w-full py-10 px-5 
   flex flex-row justify-end items-center"
     >
-      <Button
-        onClick={() => {
-          alert("test");
-        }}
-      >
-        Create Post
-      </Button>
+      <Button>Create Post</Button>
     </div>
   );
 };
